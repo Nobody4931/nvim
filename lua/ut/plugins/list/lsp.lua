82f34aaabd3aa295b4ba72e0779685654455ade8
Nobody4931 <ardenxie1@gmail.com>
2023-12-17 03:06:43 -0500

    feat: completely revamp lsp plugins
    Reorganized nvim-lspconfig plugin specification.
    Added lua_ls config plugin neodev.nvim.
    Added signature help plugin lsp_signature.nvim.
    Added LSP progress visualization plugin fidget.nvim.

--- lua/ut/plugins/list/lsp.lua
+++ lua/ut/plugins/list/lsp.lua
@@ -1,12 +1,15 @@
 return {
+	-- Package manager for Neovim that can automatically install LSP servers, linters, formatters, etc.
 	{
 		"williamboman/mason.nvim",
 
+		build = ":MasonUpdate",
+
+		event = "VeryLazy",
 		cmd = "Mason",
 
 		opts = {
-			install_root_dir = vim.fn.stdpath("data") .. "/mason",
-			max_concurrent_installers = 2,
+			max_concurrent_installers = 3,
 
 			ui = {
 				border = "none",
@@ -18,25 +21,33 @@ return {
 					package_pending = "",
 					package_uninstalled = "",
 				},
+
+				check_outdated_packages_on_open = false,
 			},
 		},
 
-		---@diagnostic disable-next-line: unused-local
-		config = function(_plugin, opts)
+		config = function(_, opts)
 			require("mason").setup(opts)
-		end
+		end,
 	},
 
+	-- Configuration tool for the native Neovim LSP client
 	{
 		"neovim/nvim-lspconfig",
 
 		dependencies = {
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
+
+			"folke/neodev.nvim",
+
 			"hrsh7th/cmp-nvim-lsp",
+			"ray-x/lsp_signature.nvim",
+
+			"j-hui/fidget.nvim",
 		},
 
-		event = "BufReadPre",
+		event = "BufReadPost",
 
 		opts = {
 			-- Options passed to `vim.diagnostic.config()`
@@ -48,28 +59,21 @@ return {
 				severity_sort = true,
 			},
 
-			-- Options passed to `nvim_lsp[server].setup()`
+			-- Options passed to `lspconfig[server].setup()`
 			servers = {
-				rust_analyzer = {},
-				gopls = {},
-				clangd = {},
-				tsserver = {},
-				pylsp = {},
 				lua_ls = {
 					settings = {
 						Lua = {
-							runtime = { version = "LuaJIT" },
-							diagnostics = { globals = { "vim" } },
-							workspace = { library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false },
-							telemetry = { enable = false },
-						}
+							workspace = {
+								checkThirdParty = false,
+							},
+						},
 					},
 				},
 			},
 		},
 
-		---@diagnostic disable-next-line: unused-local
-		config = function(_plugin, opts)
+		config = function(_, opts)
 			-- Diagnostic configuration
 			vim.diagnostic.config(opts.diagnostic)
 
@@ -77,14 +81,55 @@ return {
 			local lspconfig = require("lspconfig")
 			local mason_lspconfig = require("mason-lspconfig")
 
-			local mappings = require("ut.plugins.list.lsp.mappings")
-			local colors = require("ut.plugins.list.lsp.colors")
-			local function on_attach(...)
-				mappings.on_attach(...)
-				colors.on_attach(...)
-			end
+			local lsp_signature = require("lsp_signature")
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = cmp_nvim_lsp.default_capabilities()
+
+			local function on_attach(client, bufnr)
+				lsp_signature.on_attach(client, bufnr)
+
+				-- Disable semantic token highlighting
+				-- client.server_capabilities["semanticTokensProvider"] = nil
+
+				-- Mapping configuration
+				local map = vim.keymap.set
+				local map_opt = { buffer = bufnr }
+
+				-- Hover mappings
+				map("n", "K", vim.lsp.buf.hover, map_opt)
+				map("n", "<leader>k", vim.lsp.buf.signature_help, map_opt)
+
+				-- Goto symbols mappings
+				map("n", "<leader>lr", function() require("telescope.builtin").lsp_references(require("telescope.themes").get_dropdown()) end, map_opt)
+				map("n", "<leader>ld", function() require("telescope.builtin").lsp_definitions(require("telescope.themes").get_dropdown()) end, map_opt)
+				map("n", "<leader>lD", vim.lsp.buf.declaration, map_opt) -- lol
+				map("n", "<leader>li", function() require("telescope.builtin").lsp_implementations(require("telescope.themes").get_dropdown()) end, map_opt)
+				map("n", "<leader>lt", function() require("telescope.builtin").lsp_type_definitions(require("telescope.themes").get_dropdown()) end, map_opt)
+				map("n", "<leader>ls", function() require("telescope.builtin").lsp_document_symbols() end, map_opt)
+
+				map("n", "<leader>lw", function()
+					vim.ui.input({ prompt = "Find Symbol: " }, function(input)
+						if input then
+							require("telescope.builtin").lsp_workspace_symbols({ query = input })
+						end
+					end)
+				end, map_opt)
+				map("n", "<leader>lW", function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end, map_opt)
+
+				-- Code actions mappings
+				map("n", "<leader>ln", vim.lsp.buf.rename, map_opt)
+				map("n", "<leader>lc", vim.lsp.buf.code_action, map_opt)
+
+				-- Goto diagnostics mappings
+				map("n", "<leader>lg", vim.diagnostic.open_float, map_opt)
+				map("n", "<leader>lG", function() require("telescope.builtin").diagnostics() end, map_opt)
+
+				map("n", "]d", function() vim.diagnostic.goto_next() end, map_opt)
+				map("n", "[d", function() vim.diagnostic.goto_prev() end, map_opt)
+				map("n", "]D", function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end, map_opt)
+				map("n", "[D", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end, map_opt)
+			end
 
 			local ensure_installed = {}
 			for server in pairs(opts.servers) do
@@ -93,15 +138,76 @@ return {
 
 			mason_lspconfig.setup({ ensure_installed = ensure_installed })
 			mason_lspconfig.setup_handlers({
-				function(server)
-					local server_opts = opts.servers[server]
+				function(server_name)
+					local server_opts = opts.servers[server_name]
 					server_opts.root_dir = vim.loop.cwd
 					server_opts.on_attach = on_attach
 					server_opts.capabilities = capabilities
 
-					lspconfig[server].setup(server_opts)
+					lspconfig[server_name].setup(server_opts)
 				end
 			})
-		end
+		end,
+	},
+
+	-- Automatic Lua language server setup for Neovim's Lua API
+	{
+		"folke/neodev.nvim",
+
+		opts = {
+			setup_jsonls = false,
+		},
+
+		config = function(_, opts)
+			require("neodev").setup(opts)
+		end,
+	},
+
+	-- Shows function signatures while typing
+	{
+		"ray-x/lsp_signature.nvim",
+
+		-- TODO: Figure out why there's no separating line between the
+		-- function signature and the documentation
+		opts = {
+			handler_opts = {
+				border = "none",
+			},
+
+			doc_lines = 100,
+			max_height = 25,
+			max_width = 120,
+
+			close_timeout = 500,
+
+			hint_enable = false,
+		},
+
+		config = function(_, opts)
+			require("lsp_signature").setup(opts)
+		end,
+	},
+
+	-- Shows loading progress for LSP servers
+	{
+		"j-hui/fidget.nvim",
+
+		opts = {
+			progress = {
+				display = {
+					done_icon = "",
+				},
+			},
+
+			notification = {
+				window = {
+					winblend = 0,
+				},
+			},
+		},
+
+		config = function(_, opts)
+			require("fidget").setup(opts)
+		end,
 	},
 }
--- lua/ut/plugins/list/lsp/colors.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
----@diagnostic disable-next-line: unused-local
-function M.on_attach(client, _bufnr)
-	-- Disable LSP semantic highlighting
-	client.server_capabilities["semanticTokensProvider"] = nil
-end
-
-return M
