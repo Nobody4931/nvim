2ce663fd478c0ea88c52c0c5012d4107b1a91e9d
Nobody4931 <ardenxie1@gmail.com>
2023-12-17 05:09:03 -0500

    feat: add css support

---
+++ lua/ut/plugins/list/languages/css.lua
@@ -0,0 +1,35 @@
+return {
+  -- Add css grammars to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "css", "scss" })
+      end
+    end,
+  },
+
+  -- Add css language server to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+
+    opts = {
+      servers = {
+        cssls = {},
+      },
+    },
+  },
+
+  -- Add css formatters to conform
+  {
+    "stevearc/conform.nvim",
+
+    opts = {
+      formatters_by_ft = {
+        css = { "prettierd" },
+        scss = { "prettierd" },
+      },
+    },
+  },
+}
