3d43e9e202500f1c009258915475a0498b692006
Nobody4931 <ardenxie1@gmail.com>
2023-12-18 20:46:44 -0500

    fix: remove unnecessary dependencies list from languages importer

--- lua/ut/plugins/list/languages.lua
+++ lua/ut/plugins/list/languages.lua
@@ -1,12 +1,4 @@
 ---@type LazySpec[]
 return {
-  {
-    import = 'ut.plugins.list.languages',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'neovim/nvim-lspconfig',
-      'mfussenegger/nvim-lint',
-      'stevearc/conform.nvim',
-    },
-  },
+  { import = 'ut.plugins.list.languages' },
 }
