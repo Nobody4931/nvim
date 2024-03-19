ddb490c225060ad773aa2b3451d4ede3d174528f
Nobody4931 <ardenxie1@gmail.com>
2024-03-19 01:19:35 -0400

    doc: add todo comments for switching to none-ls (null-ls)

--- init.lua
+++ init.lua
@@ -2,5 +2,7 @@
 -- ║ ╠═  Personal Neovim Config v4
 -- ╩═╩══ https://github.com/Nobody4931
 
+-- TODO: Switch to none-ls for standardizing formatting and linting, w/ mason as a bootstrapper
+
 require('ut.native')
 require('ut.plugins')
